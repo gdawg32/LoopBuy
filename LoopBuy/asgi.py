ef447ae99d902c11d77c7a6449b25b9571e95fc8
gauresh <gnroll98@gmail.com>
2025-03-24 08:41:57 +0530

    LoopBack push

---
+++ LoopBuy/asgi.py
@@ -0,0 +1,18 @@
+"""
+ASGI config for LoopBuy project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LoopBuy.settings')
+django.setup()
+
+application = get_default_application()
